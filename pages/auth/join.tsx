5c5c25279c1cf2056d3ca07ed736ab5189f23da8
Seunghyuk <seunghyukoh0@gmail.com>
2021-12-17 19:53:04 +0900

    Update - Edit join and login - Add a naive login logic

--- pages/auth/join.tsx
+++ pages/auth/join.tsx
@@ -1,14 +1,29 @@
-import Link from 'next/link';
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export function Join() {
-  return (
-    <div>
-      <h1>Join</h1>
-      <Link href="/auth/login">
-        <a>Go to login</a>
-      </Link>
-    </div>
-  );
+	const router = useRouter();
+	const handleJoin = () => {
+		router.push("/auth/login");
+	};
+	return (
+		<div>
+			<h1>Join</h1>
+			<form onSubmit={handleJoin}>
+				<input type="text" placeholder="user id" alt="userId" required />
+				<input
+					type="password"
+					placeholder="password"
+					alt="userPassword"
+					required
+				/>
+				<button type="submit">join</button>
+			</form>
+			<Link href="/auth/login">
+				<a>Go to login</a>
+			</Link>
+		</div>
+	);
 }
 
 export default Join;
--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,21 +1,45 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { FormEvent, FormEventHandler } from "react";
 
 export function Login() {
-  const router = useRouter();
-  const handleLogin = () => {
-    router.push('/');
-  };
+	const router = useRouter();
 
-  return (
-    <div>
-      <h1>Login</h1>
-      <button onClick={handleLogin}>login</button>
-      <Link href="/auth/join">
-        <a>Join this service</a>
-      </Link>
-    </div>
-  );
+	const userId = "test";
+	const password = "password";
+
+	const handleLogin = (event: FormEvent) => {
+		event.preventDefault();
+
+		// TODO: Just for test
+		let inputUserId = event.target.userId.value;
+		let inputPassword = event.target.password.value;
+
+		if (inputUserId === userId && inputPassword === password) {
+			router.push("/");
+		} else {
+			alert("로그인 실패!\n아이디와 비밀번호를 다시 확인해주세요");
+		}
+	};
+
+	return (
+		<div>
+			<h1>Login</h1>
+			<form onSubmit={handleLogin}>
+				<input type="text" name="userId" placeholder="user id" required />
+				<input
+					type="password"
+					name="password"
+					placeholder="password"
+					required
+				/>
+				<button type="submit">login</button>
+			</form>
+			<Link href="/auth/join">
+				<a>Join this service</a>
+			</Link>
+		</div>
+	);
 }
 
 export default Login;
